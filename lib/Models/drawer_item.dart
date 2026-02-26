58aaa0595018c351b4a30e49fbdab9826b3395b6
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-02-26 13:24:38 +0200

    Add files via upload

---
+++ lib/Models/drawer_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ItemModel {
+  final String title;
+  final IconData icon;
+
+  const ItemModel({required this.title, required this.icon});
+}
---
+++ lib/widgets/custom_sliver_grid.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app/widgets/custom_item.dart';
+
+class CustomSliverGrid extends StatelessWidget {
+  const CustomSliverGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+      ),
+      itemBuilder: (context, index) {
+        return CustomItem();
+      },
+      itemCount: 4,
+    );
+  }
+}
